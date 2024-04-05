9ce8cabff9533a8170c619504455085d22a1b535
JFortun <josemanuelfortunromero@gmail.com>
2024-04-05 21:02:58 +0200

    feat(data): create PriceRepository.java and PriceResponse.java to manage data access

---
+++ src/main/java/com/fortun/backend/PriceRepository.java
@@ -0,0 +1,29 @@
+package com.fortun.backend;
+
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
+
+interface PriceRepository extends ReactiveCrudRepository<PriceResponse, Integer> {
+
+    /**
+     * Method to query the prices based on a criteria
+     *
+     * @param requestDateTime the time at which the request was made
+     * @param brandId         id of the brand
+     * @param productId       id of the product
+     * @return a reactive collection of prices that matches the query criteria
+     */
+    @Query("""
+            SELECT p.START_DATE, p.END_DATE, p.BRAND_ID, p.PRODUCT_ID, p.PRICE_LIST, p.PRICE
+            FROM PRICES p
+            WHERE p.BRAND_ID = :brandId
+            AND p.PRODUCT_ID = :productId
+            AND :requestDateTime BETWEEN p.START_DATE AND p.END_DATE
+            ORDER BY p.PRIORITY DESC
+            LIMIT 1
+            """)
+    Mono<PriceResponse> findPrices(LocalDateTime requestDateTime, int brandId, int productId);
+}
